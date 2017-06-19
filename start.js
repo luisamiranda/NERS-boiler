362237d97766751b3e2c2d6580103aa6258b5521
Luisa de Miranda <lemiranda@gmail.com>
2017-06-19 17:29:04 -0400

    working boilerplate no backend

--- start.js
+++ start.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 // routing to individual paths is done on the front end
 module.exports = app
+  .use(morgan('dev'))
+  .use(bodyParser.json())
+  .use(bodyParser.urlencoded({ extended: true }))
+
   .use(express.static(path.join(__dirname, '/public')))
-  .get('/*', (req, res, next) => res.sendFile(path.join(__dirname, '/public/index.html')))
+  .get('*', (req, res, next) => res.sendFile(path.join(__dirname, '/public/index.html')))
+  .use((req, res, next) => {
+    const err = new Error('Not found.');
+    err.status = 404;
+    next(err);
+  })
+  .use((err, req, res, next) => {
+  console.error(err);
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || 'Internal server error.');
+  })
   .listen(1337, (req, res, next) => console.log("listening on 1337"));
